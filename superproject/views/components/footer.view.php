256abc25b2de3dd292bd714f47b2d6263a4eb841
Анеля <hodanovaanela@gmail.com>
2026-06-03 17:07:34 +0300

    создала в папке views папку components и создала в ней два файла с шапкой и футером

---
+++ superproject/views/components/footer.view.php
@@ -0,0 +1,8 @@
+<footer class="d-flex flex-wrap justify-content-between align-items-center pt-3 mt-2 text-muted small">
+        <div class="col-md-4 mb-0">© <?= date('Y') ?> УЧЁТ ЛВС — система мониторинга сети</div>
+        <div class="col-md-4 d-flex justify-content-end">
+            <span><i class="bi bi-shield-check me-1"></i> Версия 1.0 | Все права защищены</span>
+        </div>
+    </footer>
+</body>
+</html>
