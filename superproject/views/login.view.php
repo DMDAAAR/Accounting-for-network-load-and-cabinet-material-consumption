04d3f6d21b5f2bc4cdabb2219b52b93d49d8cdde
stolbovayadaria <stolbovayad_a@mail.ru>
2026-06-04 14:40:47 +0400

    Update login.view.php

--- superproject/views/login.view.php
+++ superproject/views/login.view.php
@@ -1,22 +1,25 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
-    <title>авторизация</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="/superproject/views/main.css">
+    <title>Авторизация</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/superproject/views/main.css" rel="stylesheet">
 </head>
 <body>
-<form action="" method="post">
-<label>Логин</label>
-<input type="text" name="username" placeholder="Введите свой логин">
-<label>Пароль</label>
-<input type="password" name="password" placeholder="Введите пароль">
-<p>
-    у вас нет аккаунта? - <a href="../controllers/register.controller.php">зарегистрируйтесь</a>
-</p>
-<button type="submit">Войти</button>
-</form>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="card shadow">
+            <div class="card-body">
+                <form action="" method="post">
+                    <div class="alert alert-danger" role="alert" style="display: none;"></div>
+                    <label>Имя:</label>
+                    <input type="text" class="form-control" name="username" placeholder="Becquerer cloud name">
+                    <label>Пароль:</label>
+                    <input type="password" class="form-control" name="password" placeholder="Becquerer password">
+                    <button type="submit" class="btn-custom-green">Войти</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
