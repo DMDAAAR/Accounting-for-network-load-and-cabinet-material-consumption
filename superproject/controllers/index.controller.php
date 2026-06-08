39cb12c86fa0a6cd28c48a8a87a1e790a43bdb44
stolbovayadaria <stolbovayad_a@mail.ru>
2026-06-08 17:37:45 +0400

    Update index.controller.php

--- superproject/controllers/index.controller.php
+++ superproject/controllers/index.controller.php
@@ -9,7 +9,6 @@ define('APP_LOADED', true);
 
 require_once '../db/connectDB.php';
 require_once '../models/inventory.model.php';
-require_once '../models/logs.model.php';
 
 $search = isset($_GET['search']) ? trim($_GET['search']) : '';
 $limit = 5; 
@@ -19,12 +18,5 @@ $offset = ($page - 1) * $limit;
 $data = getFilteredPoints($pdo, $search, $limit, $offset);
 $totalPoints = getTotalFilteredPoints($pdo, $search);
 $totalPages = ceil($totalPoints / $limit);
-
-$log_limit = 5; 
-$log_page = isset($_GET['log_page']) ? max(1, intval($_GET['log_page'])) : 1;
-
-$logs = getRecentLogs($pdo, $log_page, $log_limit);
-$totalLogsCount = getTotalLogsCount($pdo);
-$totalLogPages = ceil($totalLogsCount / $log_limit);
 
 require '../views/index.view.php';
